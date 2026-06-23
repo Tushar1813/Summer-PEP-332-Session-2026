06d6bb4442bf09177010a1fdddc636f4e36a9bd9
Tushar1813 <tusharrisu2003@gmail.com>
2026-06-23 16:49:09 +0530

    Done things on Day 2 of Cpp

--- 1.cpp
+++ 1.cpp
@@ -1,5 +1,36 @@
 #include <iostream>
+#include <vector>
 using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node *next;
+
+    //Constructor
+    Node(int val){
+        data= val;
+        next=NULL;
+    }
+};
+
+void func1(int x){
+        cout<<"Value of x:"<< x <<endl;
+    }
+
+//Pass by value used to not change the value just make a copy of original var
+int func2(int y){
+        int ans=2*y;
+        return ans;
+    }
+
+
+//  Pass by reference  used to change the original var value
+void doubleTheValue(int &num){
+    num*=2;
+    cout<<"Inside:"<<num<<endl;
+}
+
 
 int main(){
     // int age=10;
@@ -46,15 +77,64 @@ int main(){
     //Loops:
 
     // for loop
-    for(int i=0; i<5; i++){
-        cout<<"*";
-        for(int j=0;j<3;j++){
-            if(i==0 ||i==4){
-                cout<<'*';
-            }
-            else
-            {cout<<" ";}
-        }
-        cout<<"*"<<endl;
+    // for(int i=0; i<5; i++){
+    //     cout<<"*";
+    //     for(int j=0;j<3;j++){
+    //         if(i==0 ||i==4){
+    //             cout<<'*';
+    //         }
+    //         else
+    //         {cout<<" ";}
+    //     }
+    //     cout<<"*"<<endl;
+    // }
+    // func1(3);
+    // cout<< func2(2) <<endl;
+    // int ab=2;
+    // cout<<&ab<<endl;
+    // doubleTheValue(ab);
+
+    // Pointers are same as variables ie container but contains only address of other variables
+    // int a=10;
+    // int *ptr=&a;  // we always use same datatype for pointer as of the address of whoever variable it is taking
+    // cout<<&a<<endl;
+    // cout<<ptr<<endl;
+    // cout<<*ptr<<endl;// whenever we use *ptr after that initialization it will just Dereferences itslf ie ptr  and fetch the value of place inside integer it is referencing to
+    // cout<<*(ptr+1)<<endl;//garbage value taking from after the place where  var a points 
+    // cout<<*ptr + 1<<endl; 
+    
+    //Constructor Method
+    // it is the method which is of same name as class and it will have no return type and as sooon as the object is created the construcror will be called
+    // Node *head= new Node(2);// Runtime  variable created  Dynamic memory allocation(Heap)
+
+    // Array
+    // int arr[5]= {1,2,3,4,5};
+    // for(int i: arr){
+    //     cout<< i <<endl;
+    // }
+
+    // Vectors:
+
+    vector<int> vec1={1,2,3,4,5};
+    cout<<vec1[4]<<endl;
+    cout<<vec1.at(4)<<endl;
+    cout<<"while Pushing_back 7 in vec1:"<<endl;
+    vec1.push_back(7);
+    
+    for(int i:vec1){
+        cout << i<< endl;
+    }
+    cout<<"while Pop_back vec1:"<<endl;
+
+    vec1.pop_back();
+    for(int i:vec1){
+        cout << i<< endl;
+    }
+
+
+    cout<<"After using clear() vec1 :"<<endl;
+    vec1.clear();
+    for(int i:vec1){
+        cout << i<< endl;
     }
 }
